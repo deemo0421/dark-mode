c9e44adbf317a49d5d0c05c6a30784c0efb5d413
otterrr <b211119012@gmail.com>
2022-05-06 17:57:24 +0800

    dark-mode

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AppContainer from './common/containers/App';
+import './styles/_main.scss';
+import Routes from './routes';
+import {ThemeProvider} from './global/ThemeContext'
+
+ReactDOM.render(
+  <AppContainer>
+    <ThemeProvider>
+      <Routes />
+    </ThemeProvider>
+  </AppContainer>,
+  document.getElementById('root')
+);
